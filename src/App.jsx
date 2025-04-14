ef302073c35c0499aebb9e010b706ac354d757c7
Juan Torres <torrescjuan@javeriana.edu.co>
2025-04-14 12:01:31 -0500

    Styling index.css scrolling html and FrontPage stulish

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,18 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import FrontPage from './components/frontPage';
-import TiltedCard from './components/TiltedCard';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <main>
-      <Navbar />
-      <div className="h-15"></div>
-      <FrontPage />
-        <div className="container mx-auto px-4 py-8">
-          <div className="flex flex-wrap justify-center gap-10">
-{/*             <TiltedCard 
-              image="https://turismovalledelcauca.com/wp-content/uploads/2020/01/foto-florida-municipio-turismo-valle-del-cauca-colombia4.jpg"
-              title="Iglesia"
-              description="Aquí se muestra información detallada sobre la Unidad VI, su contenido histórico y cultural."
-            />
-            <TiltedCard
-              image="https://travel.valledelcauca.gov.co/storage/Clientes/Gobernacion/ValleCTravel/imagenes/contenidos/2181-Florida_Valle_del_Cauca_2E.jpg"
-              title='Casas'
-              description='Aquí se muestra información detallada sobre la Unidad VI, su contenido histórico y cultural.'
-            /> */}
-          </div>
-        </div>
-      <Footer/>
+    <main className="relative min-h-screen overflow-x-hidden">
+      {/* Fondo con gradiente usando tus variables de color */}
+      <div className="absolute -top-28 -left-0 w-full h-[500px] bg-gradient-to-tr from-[var(--color-1)]/20 to-[var(--color-10)]/20 rounded-full blur-[80px] -z-10"></div>
+      <div className="overflow-hidden">
+        <Navbar />
+        <FrontPage />
+        <Footer />
+      </div>
     </main>
   );
 }
--- src/components/FrontPage.jsx
+++ src/components/FrontPage.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import logo from '../assets/logo_bg_blanco.webp'
+import React from 'react';
+import logo from '../assets/logo_bg_blanco.webp';
 
 const FrontPage = () => {
   return (
     <section
       id="home"
-      className="pt-12 pb-12 min-h-screen w-full bg-gradient-to-r from-[var(--color-2)] to-[var(--color-5)]"
+      className="pt-27 pb-16 min-h-screen w-full"
     >
       <div className="container mx-auto h-full flex flex-col md:flex-row items-center justify-between px-4 sm:px-6 lg:px-8">
-        
         {/* Left Column */}
-        <div className="w-full md:w-1/2 space-y-6">
-          <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-[var(--color-1)]">
+        <div className="w-full md:w-1/2 space-y-8">
+          <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-[var(--color-1)]">
             Descubre la magia de Florida, Valle del Cauca
           </h1>
           <p className="text-base md:text-lg max-w-xl text-[var(--color-3)]">
@@ -21,7 +20,7 @@ const FrontPage = () => {
         </div>
 
         {/* Right Column - Imagen representativa */}
-        <div className="w-full md:w-1/2 mt-12 md:mt-0 pl-0 md:pl-12 flex justify-center">
+        <div className="w-full md:w-1/2 mt-16 md:mt-0 pl-0 md:pl-12 flex justify-center">
           <div className="relative">
             <img
               src={logo}
@@ -33,7 +32,7 @@ const FrontPage = () => {
 
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default FrontPage
+export default FrontPage;
